e18a5d8e7c14480f6897df3605395eb43fe087c1
Ahsan Awadullah Azizan <ahsan_azizan_31rpl@student.smktelkom-mlg.sch.id>
2022-10-31 21:49:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let stars = document.getElementById('star')
 let moon = document.getElementById('moon')
 let mountains_front = document.getElementById('mountains_front')
 let mountains_behind = document.getElementById('mountains_back')
-let text = document.getElementById('header-text')
+let hi_there = document.getElementById('header-text')
 let btn = document.getElementById('btn')
 
 window.addEventListener('scroll', function(){
@@ -10,7 +10,7 @@ window.addEventListener('scroll', function(){
     moon.style.top = (scroll * .9) + 'px'
     stars.style.left = scroll + 'px'
     mountains_behind.style.top = (scroll * .2) + 'px'
-    text.style.marginRight = (scroll * 2.3) + 'px'
+    hi_there.style.marginRight = (scroll * 2.3) + 'px'
     btn.style.marginTop = (scroll * 1.5) + 'px'
 })
 
