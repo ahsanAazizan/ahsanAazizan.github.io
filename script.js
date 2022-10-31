fa552b98bd62cd2337783a24736ac21cdbe82d2a
Ahsan Awadullah Azizan <ahsan_azizan_31rpl@student.smktelkom-mlg.sch.id>
2022-10-31 13:25:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let stars = document.getElementById('star')
+let moon = document.getElementById('moon')
+let mountains_front = document.getElementById('mountains_front')
+let mountains_behind = document.getElementById('mountains_back')
+let text = document.getElementById('header-text')
+let btn = document.getElementById('btn')
+
+window.addEventListener('scroll', function(){
+    let scroll = window.scrollY;
+    moon.style.top = (scroll * .9) + 'px'
+    stars.style.left = scroll + 'px'
+    mountains_behind.style.top = (scroll * .2) + 'px'
+    text.style.marginRight = (scroll * 2.3) + 'px'
+    btn.style.marginTop = (scroll * 1.5) + 'px'
+})
+
+
