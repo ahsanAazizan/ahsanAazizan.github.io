65089f66a3e371fb229502cbef58ff9f04f39775
Ahsan Awadullah Azizan <ahsan_azizan_31rpl@student.smktelkom-mlg.sch.id>
2022-11-21 13:31:27 +0700

    Update script.js
    Signed-off-by: Ahsan Awadullah Azizan <ahsan_azizan_31rpl@student.smktelkom-mlg.sch.id>

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,3 +7,12 @@ hamburger.addEventListener('click', function() {
     navMenu.classList.toggle('hidden')
 })
 
+function copyToClipboard() {
+    var copyText = document.getElementById("copy")
+
+    copyText.select()
+    copyText.setSelectionRange(0, 99999)
+
+    navigator.clipboard.writeText(copyText.value)
+    alert("Copied the text: " + copyText.value)
+}
