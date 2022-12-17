bd15a593ae638fc437d67596a88d6cdea4e81978
Ahsan Awadullah Azizan <ahsan_azizan_31rpl@student.smktelkom-mlg.sch.id>
2022-12-17 15:54:54 +0700

    Update script.js
    Signed-off-by: Ahsan Awadullah Azizan <ahsan_azizan_31rpl@student.smktelkom-mlg.sch.id>

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,8 +11,6 @@ function reveal() {
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active")
-    } else {
-      reveals[i].classList.remove("active")
     }
   }
 }
